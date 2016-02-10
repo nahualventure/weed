d92347f692287a4348b1e5c1e74de4e99f60d595
Jeancarlo Barrios <jeancarlobarrios@gmail.com>
2016-02-10 06:46:39 -0700

    added .gitignore and basic structure for directive

---
+++ src/js/navbar/navbar.js
@@ -0,0 +1,17 @@
+// Navbar
+//
+// <div we-navbar>
+//   <div we-navbar-element></div>
+//   <div we-navbar-element></div>
+//   ...
+// </div>
+
+'use strict';
+
+var weed = angular.module('weed', [
+    'ngAnimate'
+]);
+
+weed.directive('weNavbar', function(){
+  
+})
