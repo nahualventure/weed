50883d6768bc7a7f657a70b483697eae160c02dd
Alberto <albertosuri123@gmail.com>
2017-06-01 14:14:39 -0600

    calendar fixes

--- dist/assets/js/weed.js
+++ dist/assets/js/weed.js
@@ -714,10 +714,8 @@ u.left+m<0&&d.width-l.width<=u.right?i[1]="left":u.right+m<0&&d.width-l.width<=u
                   response[i].dateFormatInput = new Date(moment(response[i].deadline).format('M/D/YYYY'));
                   response[i].timeFormatInput = moment(response[i].date).format('H:mm a');
                   su.push(
-                    {
-                      response[i];
-                    }
-                  )
+                    response[i];
+                  );
                 }
                 console.log(su);
                 var done = false, date = start.clone(), monthIndex = date.month(), count = 0;
